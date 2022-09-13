f4d1852aaeafb59a844d5b1162a23e3f0f177f76
Imran Zukorlic <mr.silver020@gmail.com>
2022-09-13 18:42:34 +0200

    frst try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [finale, setFinale] = useState("");
+  const [result, setResult] = useState([]);
+  const getApi = () => {
+    axios
+      .get(`https://imdb-api.com/en/API/SearchTitle/k_12345678/${finale}`)
+      .then((res) => {
+        setResult(res.data);
+        console.log(res.data);
+      });
+  };
+  React.useEffect(() => {
+    getApi();
+  }, [finale]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField
+        helperText="Please enter movie or serie name"
+        id="text"
+        label="Name"
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+      />
+      <Button
+        variant="contained"
+        onClick={() => {
+          console.log(finale);
+          setFinale(inputText);
+        }}
+      >
+        Search
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
